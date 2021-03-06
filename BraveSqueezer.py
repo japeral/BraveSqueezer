1114d64f0f1692e13304d1b252e4ffa9d67cdf82
Jose Peral <jose.peral@dana.com>
2021-03-06 01:21:46 +0000

    added autodonate feature

--- BraveSqueezer.py
+++ BraveSqueezer.py
@@ -48,6 +48,7 @@ def printMainMenu():
     print("  Type  '2'  automated loop. Open + shake search 5 times 5 minutes + Close all ")
     print("  Type  '3'  automated loop. Open once, shake + search, close once.")
     print("  Type  '4'  Solve monthly Captcha on %d browsers" %numberOfBrowsers)  
+    print("  Type  '5'  Donate BAT balances")      
     print("  Type 'esc' to terminate this program")
 
 def printRecordingMenu():
@@ -166,12 +167,12 @@ def shakeMouse():
         print("Done")               
 
 def searchSomething(url):
-    print("Searching something in the browser...")                
+    print("  Searching something in the browser...")                
     pyautogui.moveTo(searchBar_x,searchBar_y,duration=1)
     pyautogui.click(searchBar_x, searchBar_y, clicks=1, interval=0.1, button='left')    
     pyautogui.write(url, interval=0.05)
     pyautogui.press('enter')
-    print("Done")
+    print("  Done")
 
 def clickAdsNotifications():
     print("Clicking on the Ads Notifications...")
@@ -379,34 +380,41 @@ while(True):
     if(keyboard.is_pressed('4')):
         print("Solving the monthly captcha...")
         cwd_backup = os.getcwd()
-        for i in range(29,numberOfBrowsers):
+        for i in range(1,numberOfBrowsers):
             os.chdir ('C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application\\')         
             command = 'brave.exe --profile-directory="Profile %d"' % i
             os.system(command)
-            while(psutil.cpu_percent() > 90.0):
-                pass
             time.sleep(0.5)
             pyautogui.press('esc')         #to close the 'Restore pages?' popup
             time.sleep(0.5)                
             pyautogui.hotkey('win','up')   #Maximize all browser windows to align the [x] buttons on the same coordinates.
             time.sleep(0.5)        
-            print("  Profile %d launched:" % i )
+            print(">Profile %d launched:" % i )
             searchSomething("brave://rewards")
             time.sleep(2)   #Important to wait for the rewards to fully load.            
 
             #check if the orange rewards button is present
             os.chdir (cwd_backup)
-            rewardsButtonLocation = pyautogui.locateOnScreen('claim.png')
+            location = pyautogui.locateOnScreen('claim.png')
             try:                    
-                rewardsButtonCenter = pyautogui.center(rewardsButtonLocation)
-                x,y=rewardsButtonCenter
-                pyautogui.click(x, y, clicks=1, interval=1, button='left')
+                center = pyautogui.center(location)
+                x,y=center
+                pyautogui.click(x, y, clicks=1, interval=0.5, button='left')
                 print("  Rewards 'Claim' Button detected @ %d,%d" %(x,y) )
             except:
-                print("  Rewards 'Claim' Button is not present!, continue...")
-                continue
+                #check if the X orange rewards button is present
+                os.chdir (cwd_backup)
+                location = pyautogui.locateOnScreen('claim_x.png')
+                try:                    
+                    center = pyautogui.center(location)
+                    x,y=center
+                    pyautogui.click(x, y, clicks=2, interval=1, button='left')  # Close and reload captcha
+                    print("  Rewards 'Claim X' Button detected @ %d,%d" %(x,y) )
+                except:
+                    print("  Rewards 'Claim' or 'Claim X' buttons are not present!, continue...")
+                    continue
 
-            pyautogui.click(captchaX+200, captchaY, clicks=1, interval=1, button='left')
+            pyautogui.click(captchaX+200, captchaY, clicks=1, interval=0.5, button='left')
             pyautogui.vscroll(200)  #click in needed before of scrolling
             time.sleep(1)    
 
@@ -417,24 +425,26 @@ while(True):
             target=pyperclip.paste()
             print("  Detected target: %s" %target)
             time.sleep(0.5)  
-            pyautogui.click(captchaX+200, captchaY, clicks=1, interval=1, button='left')
+            pyautogui.click(captchaX+200, captchaY, clicks=1, interval=0.5, button='left')
 
             xSnapShift=180
             ySnapShift=50
-            img=pyautogui.screenshot("captcha_snap.png", region=(int(captchaX)-xSnapShift,int(captchaY)-ySnapShift,350,375))
+            xSnapSize=350
+            ySnapSize=375
+            img=pyautogui.screenshot("captcha_snap.png", region=(int(captchaX)-xSnapShift,int(captchaY)-ySnapShift,xSnapSize,ySnapSize))
             open_cv_image = numpy.array(img)
             open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2RGB)
+
+            # Mask text
+            cv2.rectangle(open_cv_image,  
+                         (int(xSnapSize/5*2),int(0)),           #upper left corner
+                         (int(xSnapSize),int(ySnapSize/5)), #bottom right corner
+                         (255,255,255), -1)                     # color, fill solid
+
             gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
-            blurred = cv2.GaussianBlur(gray, (25, 25), 0)
-            thresh = cv2.threshold(blurred, 195, 255, cv2.THRESH_BINARY_INV)[1]
-            #25-25-175 triangle falla
-            #23-23-175 triangle falla
-            #23-23-180 square falla
-            #23-23-185 triangle falla
-            #23-23-190 square falla
-            #23-23-195 triangle falla
-            #23-23-200 triangle falla
-            #23-23-205 square falla, circle ok,
+            blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+            thresh = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY_INV)[1]
+
             cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = imutils.grab_contours(cnts)
             
@@ -504,34 +514,39 @@ while(True):
                     print("  red triangle detected at %d,%d" %(redtriangle_X,redtriangle_Y))
 
                 if(i==3):
-                    # Uncoment to Debug image procesing
-                    cv2.imshow("Image", open_cv_image)  # show the image with 4 shapes detected
-                    cv2.waitKey(0)
                     # Discard the rest of the contours
                     break   
                 i=i+1
+
+            # Uncoment to Debug image procesing
+            #cv2.imshow("gray", gray) 
+            #cv2.imshow("blurred", blurred) 
+            #cv2.imshow("thresh", thresh) 
+            #cv2.imshow("shape detection", open_cv_image)  # show the image with 4 shapes detected
+            #cv2.waitKey(0)
 
             # drag from red triangle and drop on target.
             if(redtriangle_X!=0 and redtriangle_Y!=0 and       #check if all the figures got detected
                     circle_X!=0 and      circle_Y!=0 and
                   triangle_X!=0 and    triangle_Y!=0 and
                     square_X!=0 and      square_X!=0     ):
-                print("Red Triangle shape detected!, solving the captcha...")
-                pyautogui.moveTo(redtriangle_X,redtriangle_Y,duration=4,tween=pyautogui.easeInOutElastic)
-                print("red triangle grabbed!")
+                print("  Red Triangle shape detected!, solving the captcha...")
+                pyautogui.moveTo(redtriangle_X,redtriangle_Y,duration=1,tween=pyautogui.easeInOutElastic)
+                print("  Red Triangle grabbed!")
                 if  target == "circle ":
-                    print("dropping into the circle target...")
-                    pyautogui.dragTo(circle_X, circle_Y, duration=3,button='left',tween=pyautogui.easeInOutElastic)
+                    print("  dropping into the circle target...")
+                    pyautogui.dragTo(circle_X, circle_Y, duration=1,button='left',tween=pyautogui.easeInOutElastic)
                 if target == "square ":
-                    print("dropping into the square target...")                 
-                    pyautogui.dragTo(square_X, square_Y, duration=3,button='left',tween=pyautogui.easeInOutElastic)              
+                    print("  dropping into the square target...")                 
+                    pyautogui.dragTo(square_X, square_Y, duration=1,button='left',tween=pyautogui.easeInOutElastic)              
                 if target == "triangle ":
-                    print("dropping into the triangle target...")                                
-                    pyautogui.dragTo(triangle_X, triangle_Y, duration=3,button='left',tween=pyautogui.easeInOutElastic)                
-                print("Captcha solved!")
+                    print("  dropping into the triangle target...")                                
+                    pyautogui.dragTo(triangle_X, triangle_Y, duration=1,button='left',tween=pyautogui.easeInOutElastic)                
+                print("  Captcha solved!")
             else:
-                print("Error! one or more shapes where not properly detected.")
-            time.sleep(2)
+                print("  Error! one or more shapes where not properly detected, continue...")
+                continue
+            time.sleep(2)  #important, give time for the OK button to appear.
 
             #click in the OK button
             os.chdir (cwd_backup)
@@ -539,13 +554,79 @@ while(True):
             try:                    
                 rewardsButtonCenter = pyautogui.center(rewardsButtonLocation)
                 x,y=rewardsButtonCenter
-                pyautogui.click(x, y, clicks=1, interval=2, button='left')
-                print("Rewards 'OK' Button detected @ %d,%d" %(x,y) )
+                pyautogui.click(x, y, clicks=1, interval=1, button='left')
+                print("  Rewards 'OK' Button detected @ %d,%d" %(x,y) )
             except:
-                print("Rewards 'OK' Button is not present!, continue...")
+                print("  Rewards 'OK' Button is not present!, continue...")
+                continue
+            time.sleep(0.5)
+            pyautogui.hotkey('ctrl','w')  #close browaser tab
+
+        printMainMenu()
+
+
+    if(keyboard.is_pressed('5')):
+        print("Donating all the BAT balances...")
+        cwd_backup = os.getcwd()
+        for i in range(8,numberOfBrowsers):
+            os.chdir ('C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application\\')         
+            command = 'brave.exe --profile-directory="Profile %d"' % i
+            os.system(command)
+            time.sleep(0.5)
+            pyautogui.press('esc')         #to close the 'Restore pages?' popup
+            time.sleep(0.5)                
+            pyautogui.hotkey('win','up')   #Maximize all browser windows to align the [x] buttons on the same coordinates.
+            time.sleep(0.5)        
+            print(">Profile %d launched:" % i )
+            searchSomething("www.github.com/japeral")
+            time.sleep(2)   #Important to wait for the rewards to fully load.            
+
+            os.chdir (cwd_backup)
+
+            #click in the Donate Triangle button
+            try:
+                rewardsButtonLocation = pyautogui.locateOnScreen('donate_triangle.png')
+                rewardsButtonCenter = pyautogui.center(rewardsButtonLocation)
+                x,y=rewardsButtonCenter
+                pyautogui.click(x, y, clicks=1, interval=1, button='left')
+            except:
+                rewardsButtonLocation = pyautogui.locateOnScreen('donate_triangle_bluetick.png')
+                rewardsButtonCenter = pyautogui.center(rewardsButtonLocation)
+                x,y=rewardsButtonCenter
+                pyautogui.click(x, y, clicks=1, interval=1, button='left')
             time.sleep(0.5)
 
-        printManualMenu()
+            #click in the Send A Tip button
+            rewardsButtonLocation = pyautogui.locateOnScreen('send_a_tip.png')
+            rewardsButtonCenter = pyautogui.center(rewardsButtonLocation)
+            x,y=rewardsButtonCenter
+            pyautogui.click(x, y, clicks=1, interval=1, button='left')
+            time.sleep(0.5)
+
+            #click in the 5BAT button
+            try:
+                rewardsButtonLocation = pyautogui.locateOnScreen('5BAT.png')
+                rewardsButtonCenter = pyautogui.center(rewardsButtonLocation)
+                x,y=rewardsButtonCenter
+                pyautogui.click(x, y, clicks=1, interval=1, button='left')
+                time.sleep(0.5)
+            except:
+                print("5BAT button not detected...")
+
+            #click in the 5BAT button
+            try:
+                rewardsButtonLocation = pyautogui.locateOnScreen('SendTipButton.png')
+                rewardsButtonCenter = pyautogui.center(rewardsButtonLocation)
+                x,y=rewardsButtonCenter
+                pyautogui.click(x, y, clicks=1, interval=1, button='left')
+                time.sleep(0.5)
+            except:
+                print("Not enough Balance... continue")
+
+            pyautogui.hotkey('ctrl','w')  #close browaser tab
+
+        printMainMenu()
+
 
     if(keyboard.is_pressed('esc')):
         print("Bye bye")
